2fb2ae8f1cdffb6500405e1467b5cc6201550c2b
KritzHeng <mr.kriskanin@gmail.com>
2022-09-29 20:03:57 +0700

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import { Web3Storage } from "web3.storage";
+
+function getAccessToken() {
+  return process.env.REACT_APP_WEB3STORAGE_TOKEN
+}
 
 function App() {
+  const [fileUrl, setFileUrl] = useState(``);
+  const web3Storage = new Web3Storage({
+    token: 
+    getAccessToken(),
+ });
+  async function onChange(e) {
+    const fileInput = document.querySelector('input[type="file"]');
+    // const file = e.target.files[0]
+    // console.log(file)
+    try {
+      const cid = await web3Storage.put(fileInput.files, {
+        name: "cat pics",
+      });
+      console.log(cid);
+      console.log("2");
+    } catch (error) {
+      console.log("Error uploading file:", error);
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>IPFS</h1>
+      <input type="file" onChange={onChange} />{" "}
+      {fileUrl && <img src={fileUrl} width="600px" />}
+      <h1>{fileUrl}</h1>
     </div>
   );
 }
